5b15042a9edc13bb0a6a426e2bcfe2dbf5c114d7
unknown <testingmexico1@gmail.com>
2023-06-27 16:03:54 -0600

    Se suben archivos 01EE

---
+++ 01EE/testCase/nuevoregistro.py
@@ -0,0 +1,90 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from common.log import Log
+from common.globalparam import img_path
+import os
+import time
+
+
+class nuevoregistro:
+
+    def nuevoregistro(self, conditions, Configuracion):
+        """Abre la ventana para crear un nuevo registro"""
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Valida que la pantalla ejecutada para crear un nuevo registro sea correcta
+        try:
+            Crea = self.wait.until(conditions.visibility((By.XPATH, Configuracion.titulo_nuevo))).text
+            self.assertEqual("ESTADOS EXHIBIDOR : PROPIEDADES", Crea, "La pantalla ejecutada es correcta")
+            Log().info(" Se abrio la pantalla para el ingreso de un registro nuevo.")
+
+        except Exception as e:
+            Log().error("La pantalla ejecutada no es correcta, revise si el xpath sigue siendo el mismo, para mas detalles del error consulte el reporte")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se realiza el ingreso de datos en los campos.
+
+        try:
+            Ccodigo = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_codigo)))
+            Ccodigo.send_keys(Configuracion.Icodigo)
+            Log().info(" Ingresa el codigo del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar el codigo, revise si el xpath sigue siendo el mismo, para mas detalles del error consulte el reporte")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Cdescripcion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_descripcion)))
+            Cdescripcion.send_keys(Configuracion.Idescripcion)
+            Log().info(" Ingresa la descripción del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la descripcion, revise si el xpath sigue siendo el mismo, para mas detalles del error consulte el reporte")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Ccodigoalter = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_codigoalter)))
+            Ccodigoalter.send_keys(Configuracion.Icodigoalter)
+            Log().info(" Ingresa el codigo alternativo del nuevo registro ")
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar el codigo alternativo, revise si el xpath sigue siendo el mismo, para mas detalles del error consulte el reporte")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Guarda = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Guarda)))
+            Guarda.click()
+            Log().info(" Se da clic en el boton Guardar; se debe crear un nuevo registro.")
+
+        except Exception as e:
+            Log().error("No se encontró el botón Guardar, revise si el xpath sigue siendo el mismo, para mas detalles del error consulte el reporte")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
