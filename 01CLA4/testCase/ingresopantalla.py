20772078af6dde4493a13c1afaa1d273d700b678
unknown <testingmexico1@gmail.com>
2023-06-27 14:58:13 -0600

    Se suben archivos 01CLA4

---
+++ 01CLA4/testCase/ingresopantalla.py
@@ -0,0 +1,164 @@
+from importlib.machinery import SourceFileLoader
+from selenium.common import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from common.log import Log
+from common.globalparam import img_path
+import os
+from selenium.webdriver import ActionChains
+import time
+
+buscador = SourceFileLoader("buscador", "C:/xampp/htdocs/versiones/automatizaciones/AutoPWST/buscador.py").load_module()
+Buscador = buscador.buscador
+
+class ingresopantalla:
+
+    def ingresopantalla(self, conditions, Configuracion):
+
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Selecciona el menu
+        try:
+            Tablas = self.wait.until(conditions.visibility((By.XPATH, "//*[@id='_tablas']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .click(Tablas) \
+                .pause(0) \
+                .release()
+            action.perform()
+
+            Articulos = self.wait.until(conditions.visibility((By.XPATH, "//*[@id='_tablas_articulo']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .move_to_element(Articulos) \
+                .pause(0) \
+                .release()
+            action.perform()
+
+            ClasArt4 = self.wait.until(conditions.visibility((By.XPATH, "//*[@id='clasart4']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .click(ClasArt4) \
+                .pause(0) \
+                .release()
+            action.perform()
+            Log().info(" Abre la pantalla de clasificación de artículos 4")
+
+        except (NoSuchElementException, TimeoutException) as e:
+            Buscador.buscador(self)
+            Log().info(" La función buscador funciona de manera correcta.")
+
+        """Abre pantalla de listado de registros."""
+        # Valida que la pantalla ejecutada sea correcta
+        try:
+            Pantalla = self.wait.until(conditions.visibility((By.XPATH, Configuracion.titulo_pantalla))).text
+            self.assertEqual("CLASIFICACIÓN DE ARTÍCULO 4", Pantalla, "La pantalla ejecutada es correcta")
+            Log().info(" La pantalla ejecutada es Clasificación de Artículo.")
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+
+        except Exception as e:
+            Log().error("La pantalla ejecutada no es correcta, validar que la acción anterior haya finalizado,"
+                        "que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(2)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Condicion si el registro existe
+
+        try:
+            Registrorepetido = self.driver.find_element(By.XPATH, "//span[text()='A10']")
+            if Registrorepetido.is_displayed():
+                try:
+                    action = ActionChains(self.driver)
+                    action \
+                        .move_to_element(Registrorepetido) \
+                        .pause(0) \
+                        .click(Registrorepetido) \
+                        .pause(0) \
+                        .release()
+                    action.perform()
+                    Log().info(" Se da clic en el registro creado, para proceder a eliminarlo.")
+
+                except Exception as e:
+                    Log().error("No se encontró el registro, validar que la acción anterior haya finalizado,"
+                                "que el xpath sea el correcto o que la página no presente lentitud")
+                    time.sleep(2)
+                    timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+                    img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+                    Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+                    self.driver.get_screenshot_as_file(img_name)
+                    return False
+
+                try:
+                    Elimina = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Elimina)))
+                    Elimina.click()
+                    Log().info(" Se presiona el boton 'Eliminar', para eliminar el registro.")
+
+                except Exception as e:
+                    Log().error("No se dio click al botón Eliminar, validar que la acción anterior haya finalizado,"
+                                "que el xpath sea el correcto o que la página no presente lentitud")
+                    time.sleep(2)
+                    timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+                    img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+                    Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+                    self.driver.get_screenshot_as_file(img_name)
+                    return False
+
+                try:
+                    Confirma_Elimina = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_acepta_eliminar)))
+                    Confirma_Elimina.click()
+                    Log().info(" Se confirma el eliminado del registro")
+
+                except Exception as e:
+                    Log().error("No se dio click el botón Aceptar, validar que la acción anterior haya finalizado,"
+                                "que el xpath sea el correcto o que la página no presente lentitud")
+                    time.sleep(2)
+                    timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+                    img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+                    Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+                    self.driver.get_screenshot_as_file(img_name)
+                    return False
+
+                try:
+                    Refresca4 = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Refresca)))
+                    Refresca4.click()
+                    Log().info(" Se presiona el boton 'Refrescar', para crear un nuevo registro igual al anterior.")
+                    time.sleep(2)
+
+                except Exception as e:
+                    Log().error("No se dio click el botón Refrescar, validar que la acción anterior haya finalizado,"
+                        "que el xpath sea el correcto o que la página no presente lentitud")
+                    time.sleep(2)
+                    timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+                    img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+                    Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+                    self.driver.get_screenshot_as_file(img_name)
+                    return False
+
+        except NoSuchElementException:
+            pass
+
+        # Da clic en nuevo
+        try:
+            Nuevo = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Nuevo)))
+            Nuevo.click()
+            Log().info(" Se presiona el boton 'Nuevo', para crear un nuevo registro.")
+
+        except Exception as e:
+            Log().error("No se dio click al botón Nuevo, validar que la acción anterior haya finalizado,"
+                        "que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(2)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
