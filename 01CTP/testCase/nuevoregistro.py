f7561b4cc5517417dc51b2ffe2c5b3f76930905e
unknown <testingmexico1@gmail.com>
2023-06-27 15:46:13 -0600

    Se suben archivos 01CTP

---
+++ 01CTP/testCase/nuevoregistro.py
@@ -0,0 +1,100 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from common.log import Log
+from common.globalparam import img_path
+import os
+import time
+
+
+class nuevoregistro:
+
+    def nuevoregistro(self, conditions, Configuracion):
+        """Abre la ventana para crear un nuevo registro"""
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Valida que la pantalla ejecutada para crear un nuevo registro sea correcta
+        try:
+            Crea = self.wait.until(conditions.visibility((By.XPATH, Configuracion.titulo_nuevo))).text
+            self.assertEqual("CUSTOM PROPERTIES : PROPIEDADES", Crea, "La pantalla ejecutada es correcta")
+            Log().info(" Se abrio la pantalla para el ingreso de un registro nuevo.")
+
+        except Exception as e:
+            Log().error("La pantalla ejecutada no es correcta, validar que la acción anterior haya finalizado, que el nombre de la pantalla sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se realiza el ingreso de datos en los campos.
+
+        try:
+            Ccodigo = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_codigo)))
+            Ccodigo.send_keys(Configuracion.Icodigo)
+            Log().info(" Ingresa el codigo del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar el codigo, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Cdescripcion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_descripcion)))
+            Cdescripcion.send_keys(Configuracion.Idescripcion)
+            Log().info(" Ingresa la descripción del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la descripcion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Caplicablea = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_aplicablea)))
+            Caplicablea.click()
+
+            registro_aplicablea = self.wait.until(conditions.visibility((By.XPATH, "//li[text()='Clientes']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .click(registro_aplicablea) \
+                .pause(0) \
+                .release()
+            action.perform()
+            Log().info(" Se dió click en la opción Clientes.")
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+
+        except Exception as e:
+            Log().error("No se dió click en la opción Clientes, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Guarda = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Guarda)))
+            Guarda.click()
+            Log().info(" Se da clic en el boton Guardar; se debe crear un nuevo registro.")
+
+        except Exception as e:
+            Log().error("No se dió click en el botón Guardar, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
