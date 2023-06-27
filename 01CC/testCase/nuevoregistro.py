df520a8607ba96ac50ef446857c15b8dbd99a1c3
unknown <testingmexico1@gmail.com>
2023-06-27 14:17:31 -0600

    Se suben cambios 01CC

---
+++ 01CC/testCase/nuevoregistro.py
@@ -0,0 +1,136 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from common.log import Log
+from common.globalparam import img_path
+import os
+import time
+
+
+class nuevoregistro:
+
+    def nuevoregistro(self, conditions, Configuracion):
+        """Abre la ventana para crear un nuevo registro"""
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Valida que la pantalla ejecutada para crear un nuevo registro sea correcta
+        try:
+            Crea = self.wait.until(conditions.visibility((By.XPATH, Configuracion.titulo_nuevo))).text
+            self.assertEqual("CENTROS COSTO : PROPIEDADES", Crea, "La pantalla ejecutada es correcta")
+            Log().info(" Se abrio la pantalla para el ingreso de un registro nuevo.")
+
+        except Exception as e:
+            Log().error("La pantalla ejecutada no es correcta, validar que la acción anterior haya finalizado, que el nombre de la pantalla sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se realiza el ingreso de datos en los campos.
+
+        try:
+            Ccodigo = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_codigo)))
+            Ccodigo.send_keys(Configuracion.Icodigo)
+            Log().info(" Ingresa el codigo del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar el codigo, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Cdescripcion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_descripcion)))
+            Cdescripcion.send_keys(Configuracion.Idescripcion)
+            Log().info(" Ingresa la descripción del nuevo registro ")
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la descripcion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Cambio de pestaña observaciones
+
+        try:
+            Bobservaciones = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_observaciones)))
+            Bobservaciones.click()
+            Log().info("Se hace el cambio de pestaña para continuar con el registro nuevo")
+
+        except Exception as e:
+            Log().error("No se dió click en el botón observaciones, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Ingresa Valores
+
+        try:
+            CObservaciones1 = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_observaciones1)))
+            CObservaciones1.send_keys(Configuracion.Iobservacion1)
+            Log().info(" Ingresa la Observación 1 del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la observación 1, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            CObservaciones2 = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_observaciones2)))
+            CObservaciones2.send_keys(Configuracion.Iobservacion2)
+            Log().info(" Ingresa la Observación 2 del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la observación 2, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            CObservaciones3 = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_observaciones3)))
+            CObservaciones3.send_keys(Configuracion.Iobservacion3)
+            Log().info(" Ingresa la Observación 3 del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la observación 3, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Guarda = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Guarda)))
+            Guarda.click()
+            Log().info(" Se da clic en el boton Guardar; se debe crear un nuevo registro.")
+
+        except Exception as e:
+            Log().error("No se dió click en el botón Guardar, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
