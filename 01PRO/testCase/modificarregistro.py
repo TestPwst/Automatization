7dc249f8cdd28938b006e189e24e3cac6a104f6b
unknown <testingmexico1@gmail.com>
2023-06-28 14:40:56 -0600

    Se suben archivo 01PRO

---
+++ 01PRO/testCase/modificarregistro.py
@@ -0,0 +1,154 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from common.log import Log
+from common.globalparam import img_path
+import os
+import time
+
+
+class modificarregistro:
+
+    def modificarregistro(self, conditions, Configuracion):
+        """Se abre la ventana para modificar el registro existente """
+
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Da clic en refrescar
+        try:
+            Refresca2 = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Refresca)))
+            Refresca2.click()
+            Log().info(" Se presiona el boton 'Refrescar', para proceder a modificar el registro.")
+            time.sleep(2)
+
+        except Exception as e:
+            Log().error("No se dió click en el botón Refrescar, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        Registro = self.wait.until(conditions.visibility((By.XPATH, "//span[text()='PRO01']")))
+
+        try:
+            action = ActionChains(self.driver)
+            action \
+                .move_to_element(Registro) \
+                .pause(0) \
+                .double_click(Registro) \
+                .pause(0) \
+                .release()
+            action.perform()
+            Log().info(" Se da clic en el registro creado, para proceder a modificarlo.")
+
+        except Exception as e:
+            Log().error("No se encontró el registro deseado, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Modifica los valores
+
+        try:
+            Cdescripcion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_descripcion)))
+            Cdescripcion.clear()
+            Cdescripcion.send_keys(Configuracion.Mdescripcion)
+            Log().info(" Se Modifica la descripción del registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para modificar la descripcion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Ccliente = self.wait.until(conditions.visibility((By.XPATH, Configuracion.ayuda_cliente)))
+            Ccliente.click()
+
+            registro_estado = self.wait.until(conditions.visibility((By.XPATH, "//span[text()='1469018']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .double_click(registro_estado) \
+                .pause(0) \
+                .release()
+            action.perform()
+            Log().info(" Se dió doble click en el registro de Cliente.")
+
+        except Exception as e:
+            Log().error("No se encontró el registro de CLiente, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Ccerrado = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_cerrado)))
+            Ccerrado.click()
+            Log().info(" Se dió click en el checkbox cerrado.")
+
+        except Exception as e:
+            Log().error("No se dió click en el checkbox cerrado, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Cambio Pestaña Observaciones
+
+        try:
+            BObsetvaciones = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_observaciones)))
+            BObsetvaciones.click()
+            Log().info(" Se dió click en el boton Observaciones.")
+
+        except Exception as e:
+            Log().error("No se dió click en el checkbox cerrado, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se ingresa los datos
+
+        try:
+            Cobservacion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_observaciones1)))
+            Cobservacion.clear()
+            Cobservacion.send_keys(Configuracion.Mobservacion1)
+            Log().info(" Ingresa la Observacion del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la Observacion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Guarda = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Guarda)))
+            Guarda.click()
+            Log().info(" Se da clic en el boton Guardar; se debe modificar la informacion del registro.")
+
+        except Exception as e:
+            Log().error("No se dió click en el botón Guardar, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
---
+++ 01PRO/testCase/nuevoregistro.py
@@ -0,0 +1,142 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from common.log import Log
+from common.globalparam import img_path
+import os
+import time
+
+
+class nuevoregistro:
+
+    def nuevoregistro(self, conditions, Configuracion):
+        """Abre la ventana para crear un nuevo registro"""
+        self.wait = WebDriverWait(self.driver, 60)
+
+        # Valida que la pantalla ejecutada para crear un nuevo registro sea correcta
+        try:
+            Crea = self.wait.until(conditions.visibility((By.XPATH, Configuracion.titulo_nuevo))).text
+            self.assertEqual("PROYECTOS : PROPIEDADES", Crea, "La pantalla ejecutada es correcta")
+            Log().info(" Se abrio la pantalla para el ingreso de un registro nuevo.")
+
+        except Exception as e:
+            Log().error("La pantalla ejecutada no es correcta, validar que la acción anterior haya finalizado, que el nombre de la pantalla sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se realiza el ingreso de datos en los campos.
+
+        try:
+            Ccodigo = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_codigo)))
+            Ccodigo.send_keys(Configuracion.Icodigo)
+            Log().info(" Ingresa el codigo del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar el codigo, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Cdescripcion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_descripcion)))
+            Cdescripcion.send_keys(Configuracion.Idescripcion)
+            Log().info(" Ingresa la descripción del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la descripcion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Ccliente = self.wait.until(conditions.visibility((By.XPATH, Configuracion.ayuda_cliente)))
+            Ccliente.click()
+
+            registro_estado = self.wait.until(conditions.visibility((By.XPATH, "//span[text()='679945']")))
+
+            action = ActionChains(self.driver)
+            action \
+                .double_click(registro_estado) \
+                .pause(0) \
+                .release()
+            action.perform()
+            Log().info(" Se dió doble click en el registro de Cliente.")
+
+        except Exception as e:
+            Log().error("No se encontró el registro de CLiente, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Ccerrado = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_cerrado)))
+            Ccerrado.click()
+            Log().info(" Se dió click en el checkbox cerrado.")
+
+        except Exception as e:
+            Log().error("No se dió click en el checkbox cerrado, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Cambio Pestaña Observaciones
+
+        try:
+            BObsetvaciones = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_observaciones)))
+            BObsetvaciones.click()
+            Log().info(" Se dió click en el boton Observaciones.")
+
+        except Exception as e:
+            Log().error("No se dió click en el checkbox cerrado, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        # Se ingresa los datos
+
+        try:
+            Cobservacion = self.wait.until(conditions.visibility((By.XPATH, Configuracion.campo_observaciones1)))
+            Cobservacion.send_keys(Configuracion.Iobservacion1)
+            Log().info(" Ingresa la Observacion del nuevo registro ")
+
+        except Exception as e:
+            Log().error("No se pudo encontrar el campo para ingresar la Observacion, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
+
+        try:
+            Guarda = self.wait.until(conditions.visibility((By.XPATH, Configuracion.btn_Guarda)))
+            Guarda.click()
+            Log().info(" Se da clic en el boton Guardar; se debe crear un nuevo registro.")
+
+        except Exception as e:
+            Log().error("No se dió click en el botón Guardar, validar que la acción anterior haya finalizado, que el xpath sea el correcto o que la página no presente lentitud")
+            time.sleep(3)
+            timeStrmap = time.strftime('%Y%m%d_%H_%M_%S')
+            img_name = os.path.join(img_path, "%s.png" % str(timeStrmap))
+            Log().info(message=" Captura: %s  screen：%s" % (img_path, os.path.split(img_name)[1]))
+            self.driver.get_screenshot_as_file(img_name)
+            return False
