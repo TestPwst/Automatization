671636e36ce49f693b2e7dafe25ee3c07eb44e54
unknown <testingmexico1@gmail.com>
2023-06-28 15:52:27 -0600

    Se suben mensajes 01TDD

---
+++ 01TDD/config.py
@@ -0,0 +1,70 @@
+import os
+import platform
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+
+def modify_name_for_windows(in_path):
+    if platform.system().lower().startswith("win"):
+        return in_path + ".exe"
+    return in_path
+
+#expected_conditions
+class conditions:
+    visibility = expected_conditions.visibility_of_element_located
+    clickable = expected_conditions.element_to_be_clickable
+
+class Configuracion:
+
+    __CHROME_DRIVER_PATH = modify_name_for_windows(os.path.join("C:\chromedriver\chromedriver"))
+
+    Icodigo = "test"
+    Idescripcion = "Prueba Automatizada"
+    Mdescripcion = "Prueba Cambio AutomatizadoM"
+    Mcodigousuario = "Cambiotest1"
+
+    #IDS Y XPATH
+
+    #Ingreso a la pantalla a automatizar
+    btn_sandwich = "//*[@id='toggleMenuBar']"
+    titulo_pantalla = "/html/body/div[3]/div[2]/ui-container/ui-window/div[1]/span[2]"
+    texto_buscador = "//div[text()='Tipos de distribuciòn']"
+    Iopcionbuscador = "Tipos de distribuciòn"
+
+    #Agregar, modificar y eliminar el registro
+    btn_Nuevo = "//*[contains(@id, 'new_element')]"
+    btn_Guarda = "//*[contains(@id, 'save_element')]"
+    btn_Refresca = "//*[contains(@id, 'refresh_element')]"
+    btn_Elimina = "//*[contains(@id, 'removecurrent_element')]"
+    btn_acepta_eliminar = "//button[text()='Aceptar']"
+    btn_cerrar_pantalla = "/html/body/div[3]/div[2]/ui-container/ui-window/div[1]/div/span[4]"
+    btn_cerrar = "//button[text()='Cerrar']"
+    btn_cerrar_ventana = "/html/body/div[3]/div[2]/ui-container/ui-window[2]/div[1]/div/span[4]"
+    titulo_nuevo = "/html/body/div[3]/div[2]/ui-container/ui-window[2]/div[1]/span[2]"
+
+    etiqueta_codigo = "//div[contains(@id, 'codigo_label_element')]"
+    etiqueta_descripcion = "//div[contains(@id, 'descripcion_label_element')]"
+    campo_codigo = "//input[@type = 'text' and contains(@id, 'codigo_element')]"
+    campo_descripcion = "//input[@type = 'text' and contains(@id, 'descripcion_element')]"
+    etiqueta_formulapedido = "//div[contains(@id, 'modopresupuesto_label_element')]"
+    campo_formulapedido = "//div[contains(@id, 'modopresupuesto_select')]"
+
+    btn_niveles = "//button[contains(@id, 'niveles_tabitem')]"
+    btn_Nuevo_niveles = "//div[contains(@id, 'add_niveles_element')]"
+    btn_Elimina_niveles = "//div[contains(@id, 'remove_niveles_element')]"
+    etiqueta_niveles_descripcion = "//div[contains(@id, 'descripcion_label_element')]"
+    etiqueta_categoria = "//div[contains(@id, 'categoria_label_element')]"
+    ayuda_categoria = "/html/body/div[3]/div[2]/ui-container/div/ui-window/div[10]/div[2]/ui-container/ui-row/ui-tabstrip/div/div[2]/ui-container/ui-row[2]/ui-lookup/span[1]"
+    btn_Guarda_niveles = "/html/body/div[3]/div[2]/ui-container/div/ui-window/div[10]/div[1]/ui-toolbar/div/span[1]/ui-toolbarbutton/div/svg"
+    selecciona = "//span[text()='Modifica C']"
+    btn_cerrar_modal = "/html/body/div[3]/div[2]/ui-container/ui-window[2]/div[1]/div/span[4]"
+    btn_error = "//div[contains(@id, 'tbPanelErrors_element')]"
+
+    @classmethod
+    def create_chrome_driver(cls, options=None):
+        if not options:
+            return webdriver.Chrome(cls.__CHROME_DRIVER_PATH)
+        else:
+            return webdriver.Chrome(
+                executable_path=cls.__CHROME_DRIVER_PATH,
+                options=options
+            )
